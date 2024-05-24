bb34f9332fca833a2b25d10d03eaeb23b26b60cc
NaufalFikriHakim <naufalfikri2903@gmail.com>
2024-05-24 10:37:21 +0700

    improve code readability

--- src/components/CancelButton.js
+++ src/components/CancelButton.js
@@ -1,8 +1,23 @@
-export default function CancelButton({clicked}){
+const divStyle = {
+  width: "80px",
+  height: "36px",
+  color: "#075985",
+  backgroundColor: "#FAFAF9",
+  fontFamily: "'inter', 'Times New Roman', Times, serif",
+  fontSize: "14px",
+  fontWeight: "600",
+  textAlign: "center",
+};
 
-    return(
-        <div onClick={clicked} className="cancel hoverable">
-            <p>Cancel</p>
-        </div>
-    )
+const pStyle = {
+  padding: "8px 16px",
+  margin: "auto",
+};
+
+export default function CancelButton({ clicked }) {
+  return (
+    <div onClick={clicked} className="hoverable" style={divStyle}>
+      <p style={pStyle}>Cancel</p>
+    </div>
+  );
 }
